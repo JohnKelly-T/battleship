64e5d948c480d1d62864c9933e20365bea128244
john-kelly <johnkellytemena@gmail.com>
2025-06-16 19:04:29 +0800

    Implement place method to pass tests

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -2,6 +2,13 @@ class Ship {
   constructor(length) {
     this.length = length;
     this.hits = 0;
+    this.startCoord = null;
+    this.orientation = null;
+  }
+
+  place(x, y, orientation) {
+    this.startCoord = [x, y];
+    this.orientation = orientation;
   }
 
   hit() {
