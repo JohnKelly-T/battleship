fe1cc5ae69630cbd147c3c7e745331a8b4777b21
john-kelly <johnkellytemena@gmail.com>
2025-06-19 17:22:55 +0800

    Implement placeShipsRandomly method

--- src/modules/player.js
+++ src/modules/player.js
@@ -4,6 +4,31 @@ class Player {
   constructor() {
     this.board = new Gameboard();
   }
+
+  placeShipsRandomly() {
+    let ships = [
+      'carrier', 
+      'battleship',
+      'destroyer',
+      'submarine', 
+      'patrolBoat'
+    ]
+
+    let orientations = ['vertical', 'horizontal'];
+
+    for (let ship of ships) {
+      let x = Math.floor(Math.random() * 10);
+      let y = Math.floor(Math.random() * 10);
+      let orientation = orientations[Math.floor(Math.random() * 2)];
+
+      // reroll coordinates if random value is invalid
+      while (this.board.placeShip(ship, x, y, orientation) !== true) {
+        x = Math.floor(Math.random() * 10);
+        y = Math.floor(Math.random() * 10);
+        orientation = orientations[Math.floor(Math.random() * 2)];
+      }
+    }
+  }
 }
 
 module.exports = { Player };
