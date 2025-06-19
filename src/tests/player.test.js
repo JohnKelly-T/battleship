59f90a78fb3511957a7b309e278a7c0f75e3ac5d
john-kelly <johnkellytemena@gmail.com>
2025-06-19 17:22:35 +0800

    Add test for placeShipsRandomly method

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -12,5 +12,35 @@ describe('Player class', () => {
     expect(
       player.board
     ).toBeInstanceOf(Gameboard);
-  })
+  });
+
+  describe('placeShipsRandomly() method', () => {
+    let player;
+
+    beforeEach(() => {
+      player = new Player();
+    }); 
+
+    test('places the correct number of ships', () => {
+      player.placeShipsRandomly();
+
+      expect(
+        player.board.ships.length
+      ).toBe(5);
+    });
+
+    test('places ships of each type', () => {
+      player.placeShipsRandomly();
+
+      expect(player.board.ships).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({ length: 5}),
+          expect.objectContaining({ length: 4}),
+          expect.objectContaining({ length: 3}),
+          expect.objectContaining({ length: 3}),
+          expect.objectContaining({ length: 2}),
+        ])
+      );
+    });
+  });
 });
