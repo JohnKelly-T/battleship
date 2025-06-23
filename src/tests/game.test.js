61921a6d27f233b3eac740dc70aeab23ae2d5441
john-kelly <johnkellytemena@gmail.com>
2025-06-23 17:15:42 +0800

    Add test for make move method

--- src/tests/game.test.js
+++ src/tests/game.test.js
@@ -3,10 +3,12 @@ import { Player } from "../modules/player.js";
 
 describe('Game class', () => {
   let game;
+  let player1;
+  let player2;
 
   beforeEach(() => {
-    let player1 = new Player();
-    let player2 = new Player();
+    player1 = new Player();
+    player2 = new Player();
 
     player1.placeShipsRandomly();
     player2.placeShipsRandomly();
@@ -27,4 +29,30 @@ describe('Game class', () => {
       ).toMatchObject(game.player2);
     })
   })
+
+  describe('make move method', () => {
+    test('makes move on correct player', () => {
+      let xMove = 1;
+      let yMove = 2;
+
+      // player 1 makes move on player 2
+      game.makeMove(xMove, yMove);
+
+      expect(
+        player2.gameboard.receivedAttacks[yMove][xMove]
+      ).not.toBeNull();
+
+      xMove = 4;
+      yMove = 5;
+
+
+      // player 2 makes move on player 1
+      game.makeMove(xMove, yMove);
+
+      expect(
+        player1.gameboard.receivedAttacks[yMove][xMove]
+      ).not.toBeNull();
+
+    });
+  });
 });
