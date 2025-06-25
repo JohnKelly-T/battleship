d9fb2431d889c8b1335a92506304d39c279b4093
john-kelly <johnkellytemena@gmail.com>
2025-06-25 15:47:17 +0800

    Initialize domController

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import './styles/reset.css';
 import './styles/global.css';
+import { DomController } from './modules/dom-controller.js';
+
+let domController = new DomController();
+domController.loadGamePage();
