793a7b476c7feceb007da2fe85f16f5ac3b12e59
john-kelly <johnkellytemena@gmail.com>
2025-07-04 18:00:49 +0800

    Remove .eslintignore and use ignores property

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,12 @@
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
 import js from "@eslint/js";
 
 export default defineConfig([
+  globalIgnores([
+      "node_modules/",
+      "dist/",
+  ]),
   { files: ["**/*.{js,mjs,cjs}"] },
   {
     files: ["**/*.{js,mjs,cjs}"],
