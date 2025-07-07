601b68174472cbccb447b69ce344850741ccac05
john-kelly <johnkellytemena@gmail.com>
2025-07-07 14:55:20 +0800

    Add mp3 format to assets and resources

--- webpack.common.js
+++ webpack.common.js
@@ -25,7 +25,7 @@ module.exports = {
         loader: "html-loader",
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif|mp3)$/i,
         type: "asset/resource",
       },
       {
